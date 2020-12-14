f9d6030f30e73bb75438ce839ffd38beb637deb2
Joel Moss <joel@developwithstyle.com>
2020-12-14 20:36:01 +0000

    Installed combustion

---
+++ config.ru
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "rubygems"
+require "bundler"
+
+Bundler.require :default, :development
+
+Combustion.path = 'test/internal'
+Combustion.initialize! :action_controller, :action_view
+run Combustion::Application
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "froxy"
 
+require 'bundler'
 require "minitest/autorun"
+
+Bundler.require :default, :test
+
+Combustion.path = 'test/internal'
+Combustion.initialize! :action_controller, :action_view do
+  config.hosts << 'www.example.com'
+end
