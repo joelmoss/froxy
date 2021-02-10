98a102614640f4903f5d6b885b40562820eb7a98
Joel Moss <joel@developwithstyle.com>
2021-02-10 00:07:58 +0000

    Mark CSS imports from JS as external

--- test/internal/app/views/pages/home.js
+++ test/internal/app/views/pages/home.js
@@ -1,6 +1,6 @@
 import confetti from "https://cdn.skypack.dev/canvas-confetti";
 import "./_some.css";
-// import "/lib/some.css";
+import "/lib/some.css";
 
 console.log("app/views/pages/home.js");
 
