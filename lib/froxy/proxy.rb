08c2df989788441c59793ef7947a704eca3be6ff
Joel Moss <joel@developwithstyle.com>
2021-02-23 20:56:09 +0000

    feat: Log benchmarkable time to build

--- lib/froxy/proxy.rb
+++ lib/froxy/proxy.rb
@@ -2,10 +2,13 @@
 
 require 'open3'
 require 'rack/utils'
+require 'active_support/benchmarkable'
 
 # Proxies files to esbuild.
 module Froxy
   class Proxy
+    include ActiveSupport::Benchmarkable
+
     BUILD_PATH = 'public/froxy/build'
     CLI = File.expand_path('../../bin/froxy', __dir__)
     IMAGE_TYPES = /\.(png|gif|jpeg|jpg|svg|ico|webp|avif)$/i.freeze
@@ -34,9 +37,12 @@ module Froxy
         if /\.(js|jsx|css)$/i.match?(path_info)
           return unless (path = clean_path(path_info))
           return [404, {}, []] unless file_readable?(path)
+
           return @file_server.call(env) unless Rails.application.config.froxy.use_esbuild
 
-          return build env, req, path
+          return benchmark logging_message(req) do
+            build env, req, path
+          end
         end
       end
 
@@ -44,6 +50,10 @@ module Froxy
     end
 
     private
+
+    def logging_message(request)
+      format '[froxy] "%s" for %s at %s', request.path_info, request.ip, Time.now.to_default_s
+    end
 
     def path_to_file(env, request, path)
       ext = Pathname.new(path).extname
@@ -57,7 +67,6 @@ module Froxy
       stdout, stderr, status = Open3.capture3(CLI, Rails.root.to_s, path)
 
       if status.success?
-        Rails.logger.info "[froxy] built #{path}"
         raise "[froxy] build failed: #{stderr}" unless stderr.empty?
       else
         non_empty_streams = [stdout, stderr].delete_if(&:empty?)
@@ -78,6 +87,10 @@ module Froxy
     def clean_path(path_info)
       path = Rack::Utils.unescape_path path_info.chomp('/').delete_prefix('/')
       Rack::Utils.clean_path_info path if Rack::Utils.valid_path? path
+    end
+
+    def logger
+      Rails.logger
     end
   end
 end
